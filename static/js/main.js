7c07d7a8b202b3428d07d538a5a38c4814bbf26a
Oscar Molinar <oscarmolinar96@gmail.com>
2023-11-20 10:34:01 -0500

    Update max-width and font sizes for responsive design

--- static/js/main.js
+++ static/js/main.js
@@ -3,3 +3,25 @@ document.querySelector("tbody tr").addEventListener("click", function(e) {
     if (url)
         window.open(url, "_blank")
 })
+
+//event on change screen size
+window.addEventListener("resize", function() {
+    const width = window.innerWidth || document.documentElement.clientWidth
+    const logo = document.querySelector("a.logo")
+    const h1Main = document.querySelector("main h1")
+
+    if (width < 768) {
+        logo.classList.remove("fs-1")
+        logo.classList.add("fs-2")
+
+        h1Main.classList.remove("fs-2")
+        h1Main.classList.add("fs-3")
+    }
+    else {
+        logo.classList.remove("fs-2")
+        logo.classList.add("fs-1")
+
+        h1Main.classList.remove("fs-3")
+        h1Main.classList.add("fs-2")
+    }
+})
