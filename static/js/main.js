e71a11f01c7d5663f3c5614802b30d46b5e160e9
Oscar Molinar <oscarmolinar96@gmail.com>
2023-11-23 23:18:41 -0500

    Refactor event listener to apply to all table rows

--- static/js/main.js
+++ static/js/main.js
@@ -1,7 +1,10 @@
-document.querySelector("tbody tr").addEventListener("click", function(e) {
-    const url = e.target.parentNode.getAttribute("url")
-    if (url)
-        window.open(url, "_blank")
+const trs = document.querySelectorAll("tbody tr")
+trs.forEach((tr) => {
+    tr.addEventListener("click", function(e) {
+        const url = e.target.parentNode.getAttribute("url")
+        if (url)
+            window.open(url, "_blank")
+    })
 })
 
 updateSizes()
